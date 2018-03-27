b160e953eb4e1bac3c90eb3376601bc1d09759ba
DaeSung <anseotjd12@gmail.com>
2018-03-27 17:31:41 +0900

    "Error"

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import ReactSummernote from 'react-summernote';
+import 'react-summernote/dist/react-summernote.css'; // import styles 
+import 'react-summernote/lang/summernote-ru-RU'; // you can import any other locale 
+
+// Import bootstrap(v3 or v4) dependencies 
+import 'bootstrap/js/modal';
+import 'bootstrap/js/dropdown';
+import 'bootstrap/js/tooltip';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
