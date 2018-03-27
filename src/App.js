10bc332b53ed99c0fb5f2df623a06ef70f4933e0
DaeSung <anseotjd12@gmail.com>
2018-03-27 16:09:31 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">React-Summernote</h1>
+        </header>
+        <form className="App-intro">
+          I wanna attach summer-note here !
+        </form>
+      </div>
+    );
+  }
+}
+
+export default App;
